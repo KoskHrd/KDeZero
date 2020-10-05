2f68e9e13e2cc1eac63a42fb8d604fd5b4699fd0
k.harada <k.harada.ai@gmail.com>
2020-10-05 19:23:31 +0900

    make a 'numerical_diff' function

---
+++ steps/step04.py
@@ -0,0 +1,33 @@
+from step01 import Variable
+from step02 import Function, Square
+from step03 import Exp
+import numpy as np
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2*eps)
+
+def main1():
+    f = Square()
+    x = Variable(np.array(2.0))
+    dy = numerical_diff(f, x)
+    print(dy)
+
+def f(x):
+    A = Square()
+    B = Exp()
+    C = Square()
+    return C(B(A(x)))
+
+def main2():
+    x = Variable(np.array(0.5))
+    dy = numerical_diff(f, x)
+    print(dy)
+
+if __name__ == "__main__":
+    # main1()
+    main2()
+    pass
